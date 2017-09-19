9a46d3eea5052cf11268d8943f0acbc3e59821ba
Anjaneyulu Paramalla <anjaneyulu.paramalla@kony.com>
2017-09-20 02:52:50 +0530

    Added 1.web app Manifest file, 2.service worker, 3.cached static files, 4.icons

--- mmts.js
+++ mmts.js
@@ -2,6 +2,20 @@
  * Created by Anjaneyulu on 2/12/2017.
  */
 $(function () {
+    var swRegistration;
+    if ('serviceWorker' in navigator && 'PushManager' in window) {
+        console.log('Service Worker and Push is supported');
+        navigator.serviceWorker.register('sw.js')
+        .then(function(swReg) {
+          console.log('Service Worker is registered', swReg);
+          swRegistration = swReg;
+        })
+        .catch(function(error) {
+          console.error('Service Worker Error', error);
+        });
+    } else {
+        console.warn('Push messaging is not supported');
+    }
     var url = 'https://anji.000webhostapp.com/mmts.php';
     var connectingPaths = {
         "F_S": [
